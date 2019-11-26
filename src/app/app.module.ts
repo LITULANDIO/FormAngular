b49c9ca2515411d08126b1e145afdf418dab02d8
litusmov <litus@movcall.com>
2019-11-26 21:19:25 +0100

    create components + service + model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+//Routes
 import { AppRoutingModule } from './app-routing.module';
+//Components
 import { AppComponent } from './app.component';
+import { CreateFormComponent } from './components/create-form/create-form.component';
+import { EditFormComponent } from './components/edit-form/edit-form.component';
+import { ListUsersComponent } from './components/list-users/list-users.component';
+//Service
+import { ApiRestService } from './service/api-rest.service';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateFormComponent,
+    EditFormComponent,
+    ListUsersComponent,
+    HomeComponent
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [ApiRestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/usersInterface.ts
@@ -0,0 +1,9 @@
+export interface UsersInterface {
+    id?: number,
+    name?: string,
+    surname?: string,
+    identification?: string,
+    gender?: string,
+    email?: string,
+    password?: string
+}
